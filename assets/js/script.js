4da568dc83cd86ee9075d5d0e377a895a5ebda9b
Galiant <antonijodev@gmail.com>
2018-12-09 22:58:27 +0000

    jQuery marking completion with css class

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,4 @@
 // Check off specific todos by clicking
 $("li").click(function () {
-  //if li is grey
-  if ($(this).css("color") === "rgb(128, 128, 128)") {
-    // turn it black
-    $(this).css({
-      color: "black",
-      textDecoration: "none"
-    })
-    console.log("It is currently grey!");
-  }
-  // else
-  else {
-    // turn it grey
-    $(this).css({
-      color: "grey",
-      textDecoration: "line-through"
-    })
-  }
+  $(this).toggleClass("completed");
 });
